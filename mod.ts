75fb1715e6f40d74575e26c477a32c8eb3fa4817
denyncrawford <kiquerangelb@gmail.com>
2022-02-06 13:37:28 -0300

    updating warnings

--- mod.ts
+++ mod.ts
@@ -1,6 +1,9 @@
-import { App } from "https://github.com/astrodon/astrodon/raw/main/mod.ts";
-import { dirname, join, fromFileUrl, toFileUrl } from "https://deno.land/std/path/mod.ts";
-const __dirname = dirname(fromFileUrl(import.meta.url));
+/** 
+ * Please remember to update deno cache if you worked with astrodon before, 
+ * it is recommended to use a versioned toolchain like : x/astrodon@0.1.0-alpha.2
+ */
+
+ import { App } from "https://github.com/astrodon/astrodon/raw/main/mod.ts";
 
 /**
  * Get index is a function that returns the path to the index.html file depending on the environment.
@@ -8,13 +11,15 @@ const __dirname = dirname(fromFileUrl(import.meta.url));
  * Please feel free to change this to your own url or set the DEV environment variable to true to use the local index.html file.
  */
 
-const getIndex = () => {
+ export const getIndex = async () => {
   const isDev = Deno.env.get("DEV") == "true";
+  //deno-lint-ignore no-explicit-any
+  const isProd = (globalThis as any).astrodonProduction
 
-  if (isDev) {
-    return toFileUrl(`${join(__dirname, './renderer/src/index.html')}`).href;
+  if (isDev || isProd) {
+    return `file://${ await Deno.realPath('./renderer/src/index.html')}`;
   } else {
-    return 'https://raw.githack.com/denyncrawford/astrodon-template-demo/main/renderer/index.html' //"<your_remote_html>";
+    return `https://raw.githack.com/astrodon/astrodon/dev/examples/hello_world/renderer/index.html` //"<your_remote_html>";
   }
 };
 
