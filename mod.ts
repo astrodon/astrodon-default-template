995b723fadac01663485554dd23c1460a5c46ae9
denyncrawford <kiquerangelb@gmail.com>
2022-02-06 18:49:38 -0300

    fixing local dirname

--- mod.ts
+++ mod.ts
@@ -3,7 +3,10 @@
  * it is recommended to use a versioned toolchain like : x/astrodon@0.1.0-alpha.2
  */
 
- import { App } from "https://github.com/astrodon/astrodon/raw/main/mod.ts";
+ import { App } from "https://deno.land/x/astrodon@0.1.0-alpha.2/mod.ts";
+ import { join } from "https://deno.land/std/path/mod.ts";
+
+ const __dirname = new URL('.', import.meta.url).pathname;
 
 /**
  * Get index is a function that returns the path to the index.html file depending on the environment.
@@ -11,19 +14,19 @@
  * Please feel free to change this to your own url or set the DEV environment variable to true to use the local index.html file.
  */
 
- export const getIndex = async () => {
+ export const getIndex = () => {
   const isDev = Deno.env.get("DEV") == "true";
   //deno-lint-ignore no-explicit-any
   const isProd = (globalThis as any).astrodonProduction
 
   if (isDev || isProd) {
-    return `file://${ await Deno.realPath('./renderer/src/index.html')}`;
+    return `file://${ join(__dirname, './renderer/src/index.html') }`;
   } else {
     return `https://raw.githack.com/astrodon/astrodon/dev/examples/hello_world/renderer/index.html` //"<your_remote_html>";
   }
 };
 
-const indexPath = await getIndex();
+const indexPath = getIndex();
 
 const app = await App.new();
 
